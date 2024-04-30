33e5ac8b52d9d401c7c0e82fc767d532762cd3dc
Mariia2308 <Mariia2308@users.noreply.github.com>
2024-04-30 22:08:58 +0000

    deploy: e4a4dcbee1f7076ea533a9bf93a5715f75eb91ed

---
+++ assets/MovieDetail.module-5uuLD94V.js
@@ -0,0 +1,2 @@
+const t="_title_1mttp_1",_="_card_1mttp_6",s="_img_1mttp_13",n="_link_1mttp_19",c="_links_1mttp_28",i="_wrapper_1mttp_34",p="_btn_1mttp_39",l="_castlist_1mttp_52",m={title:t,card:_,img:s,link:n,links:c,wrapper:i,btn:p,castlist:l};export{m as c};
+//# sourceMappingURL=MovieDetail.module-5uuLD94V.js.map
